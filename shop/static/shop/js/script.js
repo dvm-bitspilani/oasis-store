5a764121700430c270b8f9a5098c183acc014300
parthosa <psarthi16@gmail.com>
2016-10-13 17:25:22 +0530

    linking and updating pages

--- shop/static/shop/js/script.js
+++ shop/static/shop/js/script.js
@@ -25,33 +25,34 @@ $('#add-cart').click(function(){
     'img': $('.product-img').attr('src')
   }
 
-  // $.ajax({
-  //   type:'POST',
-  //   url:'../../buy/',
-  //   data:data,
-  //   success:function(response){
-  //     var respData = response.data;
-  //     console.log(response,respData);
-  //     if(response.status='True'){
-  //       alert(response.message)
-  //       // add new item
-  //     }
-  //
-  //   }
-  // })
-  var newItem = $('.default__item').clone();
-  newItem.show();
-  newItem.removeClass('default__item');
-  newItem.find('.shopping-cart__item__info__title').html(data.name);
-  newItem.find('.shopping-cart__item__image img').attr('src',data.img);
-  newItem.find('.shopping-cart__item__info__price').html(parseInt(data.price)*parseInt(data.quantity));
-  newItem.find('.shopping-cart__item__info__size span').html(data.size);
-  newItem.find('.shopping-cart__item__info__color span').html(data.color);
-  newItem.find('.shopping-cart__item__info__qty span').html(data.quantity);
+  $.ajax({
+    type:'POST',
+    url:'../../buy/',
+    data:data,
+    success:function(response){
+      var respData = response.data;
+      console.log(response,respData);
+      if(response.status='True'){
+        alert(response.message)
+        // add new item
+        var newItem = $('.default__item').clone();
+        newItem.show();
+        newItem.removeClass('default__item');
+        newItem.find('.shopping-cart__item__info__title').html(data.name);
+        newItem.find('.shopping-cart__item__image img').attr('src',data.img);
+        newItem.find('.shopping-cart__item__info__price').html(parseInt(data.price)*parseInt(data.quantity));
+        newItem.find('.shopping-cart__item__info__size span').html(data.size);
+        newItem.find('.shopping-cart__item__info__color span').html(data.color);
+        newItem.find('.shopping-cart__item__info__qty span').html(data.quantity);
 
-  $('#cart-list').append(newItem);
-  // console.log('hi');
-  updateCart();
+        $('#cart-list').append(newItem);
+        // console.log('hi');
+        updateCart();
+
+      }
+
+    }
+  })
 
 });
 
