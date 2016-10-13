a7646ebb79b689c8bc32a4dc9aa0abf846312acc
ashu <jainamritanshu@gmail.com>
2016-10-13 14:32:45 +0530

    api endpoint for all items

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	url(r'^buy', views.buy, name = 'buy'),
     url(r'^product/(?P<itemid>[0-9]+)/$', views.getitem, name='getitem'),
+    url(r'^items/getall/$', views.getall, name='getall'),
 ]
